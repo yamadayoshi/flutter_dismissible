c8e06c1606fc2539f3be5c442ab380791069385c
yamadayoshi <andre_lynx@hotmail.com>
2020-11-03 13:56:49 -0300

    Dismissible svg and unique key

--- lib/components/listview_dismissible.dart
+++ lib/components/listview_dismissible.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:dismissible/components/list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class ListViewDismissible extends StatefulWidget {
@@ -27,70 +28,86 @@ class _ListViewDismissibleState extends State<ListViewDismissible> {
             ),
           ];
         },
-        body: ListView.builder(
-          itemCount: Provider.of<ListItem>(context, listen: true).getLength(),
-          itemBuilder: (context, index) => Column(
-            children: [
-              Dismissible(
-                key: Key((index + Random.secure().nextInt(8888)).toString()),
-                direction: DismissDirection.endToStart,
-                background: Container(
-                  decoration: BoxDecoration(color: Color(0xFFFFE6E6)),
-                ),
-                child: Row(
+        body: Provider.of<ListItem>(context, listen: true).getLength() == 0
+            ? Center(child: Text('No items'))
+            : ListView.builder(
+                itemCount:
+                    Provider.of<ListItem>(context, listen: true).getLength(),
+                itemBuilder: (context, index) => Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 88.0,
-                        child: AspectRatio(
-                          aspectRatio: 0.95,
-                          child: Container(
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              color: Color(0xFFF5F0FF),
-                              borderRadius: BorderRadius.circular(15),
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                    Provider.of<ListItem>(context, listen: true)
-                                        .list[index]
-                                        .urlImage),
+                    Dismissible(
+                      key: Key(
+                          (index + Random.secure().nextInt(8888)).toString()),
+                      direction: DismissDirection.endToStart,
+                      background: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFFFE6E6),
+                        ),
+                        child: Row(
+                          children: [
+                            Spacer(),
+                            SvgPicture.asset("assets/icons/Trash.svg"),
+                          ],
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              width: 88.0,
+                              child: AspectRatio(
+                                aspectRatio: 0.95,
+                                child: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFFF5F0FF),
+                                    borderRadius: BorderRadius.circular(15),
+                                    image: DecorationImage(
+                                      image: NetworkImage(Provider.of<ListItem>(
+                                              context,
+                                              listen: true)
+                                          .list[index]
+                                          .urlImage),
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(Provider.of<ListItem>(context, listen: true)
-                            .list[index]
-                            .name),
-                        Text.rich(
-                          TextSpan(
-                            text: Provider.of<ListItem>(context, listen: true)
-                                .list[index]
-                                .description,
+                          SizedBox(
+                            width: 20,
                           ),
-                        )
-                      ],
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(Provider.of<ListItem>(context, listen: true)
+                                  .list[index]
+                                  .name),
+                              Text.rich(
+                                TextSpan(
+                                  text: Provider.of<ListItem>(context,
+                                          listen: true)
+                                      .list[index]
+                                      .description,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      onDismissed: (direction) {
+                        Provider.of<ListItem>(context, listen: false)
+                            .deleteAt(index);
+
+                        print(
+                            'Left: ${Provider.of<ListItem>(context, listen: false).getLength()}');
+                      },
                     )
                   ],
                 ),
-                onDismissed: (direction) {
-                  Provider.of<ListItem>(context, listen: false).deleteAt(index);
-
-                  print(
-                      'Left: ${Provider.of<ListItem>(context, listen: false).getLength()}');
-                },
-              )
-            ],
-          ),
-        ),
+              ),
       ),
     );
   }
